6245e8157f31c53e288a9f03ecb876e2e1d1a097
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-08 20:53:35 +0000

    Correciones

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,10 @@ int d;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 d = n % 10;
-printf("Last digit of % d is % d and is",n,d);
+printf("Last digit of % d is % d ",n,d);
 if (d > 5)
 {
-printf ("greater than 5\n");
+printf ("and is greater than 5\n");
 }
 else if (d == 0)
 {
@@ -25,7 +25,7 @@ printf ("0\n");
 }
 else
 {
-printf ("less than 6 and not 0\n");
+printf ("and is less than 6 and not 0\n");
 }
 return (0);
 }
