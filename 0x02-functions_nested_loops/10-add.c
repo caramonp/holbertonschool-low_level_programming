fd227a8d5dcf3357a77e5792f186ba120bd43d1a
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-09 20:11:24 +0000

    prototipos

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - adicionar dos entradas
+ * @a: primera entrada
+ * @b: segunda entrada
+ * Return: sum of both ints
+ **/
+
+int add(int a, int b)
+{
+return (a + b);
+}
