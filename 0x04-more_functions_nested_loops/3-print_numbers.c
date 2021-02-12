bfbb5999902c12a33fe2fc61321daee6312b8f8b
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-12 17:17:52 +0000

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ * i : varible
+ * Return: 0
+*/
+
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+{
+_putchar('\n');
+}
+}
