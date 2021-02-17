80f1c66fcec53fd377b97f1f74227f4f3e350cb5
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-17 20:38:18 +0000

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,20 +4,19 @@
  *print_array -  prints n elements of an array of integers
  * @a : input
  *@n : input
-  Description: prints n digits of array
  */
 
 void print_array(int *a, int n)
 {
-  int c;
+int c;
 
-  for (c = 0; c < n; c++)
-  {
-      printf("%d", a[c]);
-        if (c != n - 1)
-        {
-          printf(", ");
-        }
-  }
-  printf("\n");
+	for (c = 0; c < n; c++)
+	{
+		printf("%d", a[c]);
+				if (c != n - 1)
+				{
+					printf(", ");
+				}
+	}
+	printf("\n");
 }
