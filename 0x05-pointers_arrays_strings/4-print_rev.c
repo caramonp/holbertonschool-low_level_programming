dee495ebfc519c6a7400cf85fc12a346c043dde2
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-17 15:37:06 +0000

    revers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_rev - prints a string, in reverse
+ *@s: input
+*/
+
+void print_rev(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{}
+for (i = i -1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar ('\n');
+
+}
