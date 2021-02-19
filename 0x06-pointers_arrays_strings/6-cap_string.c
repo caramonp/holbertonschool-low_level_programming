295e7097f5d94a2f3f002508ad7f44cfa746fee4
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-19 03:42:00 +0000

    reemplazar

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *cap_string - function that capitalizes all words of a string.
+ *@s: string
+ * Return: s
+*/
+char *cap_string(char *s)
+{
+int i, j;
+char exc[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; exc[j] != '\0'; j++)
+{
+if (s[0 >= 'a' && s[0] <= 'z'])
+{
+s[0] = s[0] - 32;
+}
+if (s[i] == exc[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+{
+s[i + 1] = s[i + 1] - 32;
+}
+}
+}
+return (s);
+}
