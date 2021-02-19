eb4091dfbb0feda4deb48729d4cc56ef6623f8c6
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-19 02:24:57 +0000

    encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *leet - function that encodes a string into 1337.
+ *@s: string
+ * Return: s
+*/
+char *leet(char *s)
+{
+int i, j;
+char *c;
+c = "aAeEoOtTlL4433007711";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if (s[i] == c[j])
+s[i] = c[j + 10];
+}
+}
+return (c);
+}
