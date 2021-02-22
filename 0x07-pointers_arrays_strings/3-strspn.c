9e0a1091abd10953b20526a2051aff119a3dac05
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-22 22:35:59 +0000

    comparar

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,19 +10,16 @@
 unsigned int _strspn(char *s, char *accept)
 {
 int i, j, k;
-
+	k = 0;
 	for (i = 0; s[i] != '\0' && s[i] != 32; i++)
 	{
-		k = 0;
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0 ; accept[j] != '\0'; j++)
 		{
-			if (accept[j] == s[i])
-			k = 1;
-		}
-		if (k == 0)
-		{
-			return (i);
+			if (s[i] == accept[j])
+			{
+				k++;
+			}
 		}
 	}
-return (j);
+	return (k);
 }
