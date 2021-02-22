56d817372d43b9a02150cec70173faf6df7e7072
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-22 22:30:01 +0000

    comparar

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *_strspn - copies memory area
+ * @s: input
+ * @accept: input
+ *Return: s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		k = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			k = 1;
+		}
+		if (k == 0)
+		{
+			return (i);
+		}
+	}
+return (j);
+}
