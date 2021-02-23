963d9ccda7a4b8aebfece1694259ced4aa456525
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-23 02:25:47 +0000

    punteros

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include "stdio.h"
+/**
+ *print_diagsums - prints the chessboard
+ *@a: input
+ *@size: int
+ *Return: 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, suma1, suma2;
+
+	suma1 = 0;
+	suma2 = 0;
+	for (i = 0; i < size ; i++)
+	{
+		for (j = 0; j < size; j++)
+		if (i == j)
+		{
+			suma1 = suma1 + a[size - i + 1];
+			printf("%d\n", suma1);
+		}
+		if (i + j == size - 1)
+		{
+			suma2 = suma2 + a[size - i - 1];
+			printf("%d \n", suma2);
+		}
+
+	}
+}
