6e0d4bae557470bb09da587fa899536b87c72161
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-02-25 03:30:45 +0000

    numeros primos

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ *define_prime -  definir formula de numeros primos
+ *@i: input
+ *@j: input
+ *Return: funtion
+ **/
+int define_prime(int i, int j)
+{
+  if (j == 1)
+  {
+    return(1);
+  }
+  if (i % j == 0)
+  {
+    return (0);
+  }
+  else
+  return (define_prime(i, j - 1));
+  
+}
+/**
+ *is_prime_number -  definir formula de numeros primos
+ *@n: input
+ *Return: funtion
+ **/
+int is_prime_number(int n)
+{
+if (n <= 1)
+{
+  return (0);
+}
+else
+return(define_prime(n, n - 1));
+}
