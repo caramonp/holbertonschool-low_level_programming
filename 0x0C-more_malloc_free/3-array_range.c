8f45ac8b9a3f281555c0bce725285e3493be19b7
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-05 01:33:34 +0000

    min y max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,19 +11,18 @@ int *array_range(int min, int max)
 int *p;
 unsigned int i, j;
 i = max - min;
-if ( min > max)
+if (min > max)
 {
-  return (NULL);
+	return (NULL);
 }
 p = malloc(sizeof(int) * (i + 1));
 if (p == NULL)
 {
-  return (NULL);
+	return (NULL);
 }
 for (j = 0; j <= i; j++, min++)
-	{
-		p[j] = min;
-	}
+{
+	p[j] = min;
+}
 	return (p);
-
 }
