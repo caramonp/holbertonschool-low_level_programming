9d7f8fb7e197c50fc8d7998914a1316544c82562
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-05 00:11:57 +0000

    llenar de ceros con calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory using malloc.
+ * @nmemb: input
+ * @size: input
+ * Return: pointer.
+**/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p;
+unsigned int i;
+if (nmemb == 0)
+{
+	return (NULL);
+}
+if (size == 0)
+{
+	return (NULL);
+}
+p = malloc(nmemb * size);
+if (p == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < (nmemb * size); i++)
+{
+	p[i] = 0;
+}
+return (p);
+}
