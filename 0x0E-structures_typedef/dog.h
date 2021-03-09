756a04a14565e108bd0dcb0c0857c4a2010c80be
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-09 03:52:01 +0000

    structures new type dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOGH
+#define DOGH
+/**
+ * struct dog - new type of structure
+ * @name : dog´s name
+ * @age: age´s name
+ * @owner: owner´s dog
+ * Return : dog
+**/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
