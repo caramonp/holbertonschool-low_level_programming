586644e16ed24a354266054e30d5d53bf72699a9
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-10 19:26:50 +0000

    print name wiht betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,18 +3,11 @@
  * print_name - funtions that prints names
  * @name: nombre
  * @f: pointer
- * Return  nothing
+ * Return : nothing
 **/
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name == '\0')
-{
-  return;
-}
-if (f == '\0')
-{
-  return;
-}
+if (name != '\0' || f != '\0')
 f(name);
 }
