e2bbb3c96fdc57c8e8eb66e12a5c23b0accacd11
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-11 20:10:03 +0000

    macro sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - 
+ * @n: numero de datos
+ * Return: 0 is n == 0
+* */
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i, suma = 0;
+va_list (a); /** variable que apunta a la lista de argumentos**/
+int j;
+if (n == 0)
+{
+return 0;
+}
+va_start (a, n); /** inicializa a, ultimo argumento **/
+for (i = 0; i < n; i++)
+{
+j = va_arg (a, int);
+suma += j;
+}
+va_end (a);
+return (suma);
+}
+
