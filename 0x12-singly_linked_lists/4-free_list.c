d10fdadf2b32aa78a208a59e45bf806e54101760
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-19 23:44:53 +0000

    free the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * free_list - funtion to free the list
+ * @head: pointer to a head to the list
+ * Return: noting
+ **/
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	if (head->next)
+	{
+		free(head->next);
+	}
+	free(head->str);
+	free(head);
+}
