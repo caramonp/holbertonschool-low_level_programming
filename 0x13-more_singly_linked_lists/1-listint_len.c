9dfce69ab76099cf26dc0e59051c86f5c1e7e941
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-23 16:38:58 +0000

    number of elements in the list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_t - function that returns the number of elements in a linked
+ * @h: pointer to a list
+ * Return: count the elements
+ **/
+size_t listint_len(const listint_t *h)
+{
+int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
