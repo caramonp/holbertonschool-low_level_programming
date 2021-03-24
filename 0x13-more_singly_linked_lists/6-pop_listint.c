f8f3c7f34a31a1fae1f02632a90b87aaf719d600
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-24 01:20:24 +0000

    delete some node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * free_listint2 - funtion to free the list
+ * @head: pointer to a head to the list
+ * Return: noting
+ **/
+int pop_listint(listint_t **head)
+{
+	int position;
+
+	if (*head == NULL || head == NULL)
+	{
+		return (0);
+	}
+	position = (*head)->n;
+	free(*head);
+	return (position);
+}
