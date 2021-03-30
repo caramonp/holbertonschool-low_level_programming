88e20c38715ffc5fb8c7b735172627058141773a
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-30 02:36:58 +0000

    create new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+* create_file -  function that creates a file
+* @filename: name of the file
+* @text_content: is a NULL terminated string to write to the file
+*Return: 1 on success, -1 on failure
+**/
+int create_file(const char *filename, char *text_content)
+{
+	int abrir, imprimir, i;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	abrir = open(filename, O_RDONLY | O_CREAT | O_RDWR);
+
+	if (abrir == -1)
+	{
+		return (-1);
+	}
+
+	for (i = 0; text_content[i] != '\0'; i++)
+	{}
+	imprimir = write(abrir, text_content, i);
+	if (imprimir == -1)
+	{
+	close(abrir);
+	return (1);
+	}
+	close(abrir);
+	return (1);
+
+}
