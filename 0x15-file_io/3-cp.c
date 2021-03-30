2294db6219e2922c153537d0903fd8e778183188
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-30 04:16:44 +0000

    copy file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * main - Program that copies the content of a file to another file.
+ * @argc: Argc.
+ * @argv: Argv
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+  char buffer[BSIZE];
+  int primero, segundo, firts, second;
+
+  if (argc != 3)
+  {
+    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+    exit(97);
+  }
+  
+  primero = open(argv[1], O_RDONLY);
+  if (primero == -1)
+  {
+    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+  }
+
+ segundo = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+  if (segundo == -1)
+  {
+    dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+  }
+  do
+  {
+    firts = read(primero, buffer, BUFSIZ);
+    second = write(segundo, buffer, BUFSIZ);
+  } while (firts > 0 && second > 0);
+  close(primero);
+  close(segundo);
+  return (0);
+}
