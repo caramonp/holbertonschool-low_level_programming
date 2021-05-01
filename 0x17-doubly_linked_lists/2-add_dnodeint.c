bfcb18273e195c8435502591df2935b96478d754
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-01 23:09:31 +0000

    rename

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_dnodeint -  add a node in the list.
+ * @head: head
+ * @n: content
+ * Return: content of new nodes
+ *
+ **/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = NULL, *tmp = NULL;
+
+	new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+
+		new->n = n;
+		new->prev = NULL;
+		tmp = *head;
+		*head = new;
+		new->next = tmp;
+		
+		if (tmp != NULL)
+			tmp->prev = *head;
+
+	return (new);
+}
