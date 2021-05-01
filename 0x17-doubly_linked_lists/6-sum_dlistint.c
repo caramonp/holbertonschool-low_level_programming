aadfd9a989cf1cb2cfd839070ffe65f812adfb25
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-01 23:26:11 +0000

    betty

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,10 +1,9 @@
-  
 #include "lists.h"
 /**
  * sum_dlistint - returns the sum of all node data
  * @head: head
- * Return: result of the sum 
- */
+ * Return: result of the sum
+**/
 int sum_dlistint(dlistint_t *head)
 {
 	size_t sum = 0;
