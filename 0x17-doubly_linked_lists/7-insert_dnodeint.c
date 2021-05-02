4551ff2699eafa34d79682e558bc70eec615f670
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-02 02:04:54 +0000

    add a ode in a index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts node at index
+ * @h: head
+ * @idx: index
+ * @n: content
+ * Return: list
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int node = 1;
+	dlistint_t *tmp = NULL, *new = NULL;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL || h == NULL)
+		return (NULL);
+
+	new->n = n;
+	tmp = *h;
+
+	while (tmp->next != NULL)
+	{
+		if (node == idx)
+		{
+			new->prev = tmp;
+			new->next = tmp->next;
+			tmp->next = new;
+			new->next->prev = new;
+		}
+		tmp = tmp->next;
+		node++;
+	}
+
+	if (node < idx)
+	{
+		free(new);
+		return (NULL);
+	}
+	return (new);
+}
