7e8d179f48e98224a6d54eafe49722556baf2c99
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-07-04 19:27:07 -0500

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,13 +2,12 @@
 """[funtion island_perimeter]
 """
 
+
 def island_perimeter(grid):
     """[funtions that returns the perimeter of the island]
 
     Args:
         grid ([integers]): [list of integers]
-        
-    Return: perimeter
     """
     water_zone = 0
     land_zone = 1
@@ -17,13 +16,13 @@ def island_perimeter(grid):
     for y, row in enumerate(grid):
         for x, column in enumerate(row):
             if column == land_zone:
-                if grid[y][x - 1] == water_zone:
+                if x == 0 or grid[y][x - 1] == water_zone:
                     perimeter += 1
-                if grid[y][x + 1] == water_zone:
+                if (x + 1) == len(row) or grid[y][x + 1] == water_zone:
                     perimeter += 1
-                if grid[y - 1][x] == water_zone:
+                if y == 0 or grid[y - 1][x] == water_zone:
                     perimeter += 1
-                if grid[y + 1][x] == water_zone:
+                if (y + 1) == len(grid) or grid[y + 1][x] == water_zone:
                     perimeter += 1
 
     return perimeter
