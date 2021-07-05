b5a80f6cbd9d9b8ec9afdcb02c880ed88065d3ac
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-07-04 19:30:17 -0500

    pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,29 +4,30 @@
 
 
 def island_perimeter(grid):
-    """[funtions that returns the perimeter of the island]
+	
+		"""[funtions that returns the perimeter of the island]
 
-    Args:
-        grid ([integers]): [list of integers]
-        
-    Return: perimeter
-    """
-    
-    
-    water_zone = 0
-    land_zone = 1
-    perimeter = 0
-    
-    for y, row in enumerate(grid):
-        for x, column in enumerate(row):
-            if column == land_zone:
-                if x == 0 or grid[y][x - 1] == water_zone:
-                    perimeter += 1
-                if (x + 1) == len(row) or grid[y][x + 1] == water_zone:
-                    perimeter += 1
-                if y == 0 or grid[y - 1][x] == water_zone:
-                    perimeter += 1
-                if (y + 1) == len(grid) or grid[y + 1][x] == water_zone:
-                    perimeter += 1
+		Args:
+				grid ([integers]): [list of integers]
+				
+		Return: perimeter
+		"""
+		
+		
+		water_zone = 0
+		land_zone = 1
+		perimeter = 0
+		
+		for y, row in enumerate(grid):
+				for x, column in enumerate(row):
+						if column == land_zone:
+								if x == 0 or grid[y][x - 1] == water_zone:
+										perimeter += 1
+								if (x + 1) == len(row) or grid[y][x + 1] == water_zone:
+										perimeter += 1
+								if y == 0 or grid[y - 1][x] == water_zone:
+										perimeter += 1
+								if (y + 1) == len(grid) or grid[y + 1][x] == water_zone:
+										perimeter += 1
 
-    return perimeter
+		return perimeter
