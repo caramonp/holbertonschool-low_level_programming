7d66ce2f72cbc91c9f7bc53d2507fed04f24bc00
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-09-01 16:56:38 -0500

    Linear sort

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search -  function that searches for a value in an array
+ * @array: arrays with integers
+ * @size: number of elements
+ * @value: value of the integer
+ * Return - integer or -1
+**/
+
+
+int linear_search(int *array, size_t size, int value)
+{
+
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i <= size - 1; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	return (-1);
+}
